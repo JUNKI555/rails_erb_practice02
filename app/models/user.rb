dd58eb961459092b4e02183fdb26c64717723811
JUNKI555 <junki555@gmail.com>
2020-12-14 15:23:25 +0900

    :construction: fix model foreign_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   generate_public_uid
   devise :authenticatable
 
-  has_one :database_authentication
+  has_one :database_authentication, foreign_key: :user_public_uid, primary_key: :public_uid
+
+  self.primary_key = :public_uid
 end
--- app/models/user/database_authentication.rb
+++ app/models/user/database_authentication.rb
@@ -1,5 +1,7 @@
 class User::DatabaseAuthentication < ApplicationRecord
   devise :database_authenticatable, :validatable
 
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_public_uid, primary_key: :public_uid
+
+  self.primary_key = :user_public_uid
 end
